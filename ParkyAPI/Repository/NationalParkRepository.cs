1a6f6e38e77635052740b195312b0b3b15864d01
Humberto Mendes <humbertoarm@gmail.com>
2021-06-04 15:44:27 -0500

    Add project files.

---
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,63 @@
+﻿using ParkyAPI.Data;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public NationalParkRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Add(nationalPark);
+            return Save();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Remove(nationalPark);
+            return Save();
+        }
+
+        public NationalPark GetNationalPark(int id)
+        {
+            return _db.NationalParks.FirstOrDefault(park => park.Id == id);
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            return _db.NationalParks.OrderBy(park => park.Name).ToList();
+        }
+
+        public bool NationalParkExists(int id)
+        {
+            return _db.NationalParks.Any(park => park.Id == id);
+        }
+
+        public bool NationalParkExists(string name)
+        {
+            return _db.NationalParks.Any(park => park.Name.ToLower().Trim() == name.ToLower().Trim());
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Update(nationalPark);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0;
+        }
+    }
+}
